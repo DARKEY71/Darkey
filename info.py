111a7a202365ca26e9083764131d156f6bc1d475
DARKEY71 <kala34184@gmail.com>
2025-05-14 15:22:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ PICS = (environ.get('PICS', 'https://graph.org/file/c79a6bb83af982d9b681e-31c8c2
 
 
 # Admins & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5857491193').split()] # For Multiple Id Use One Space Between Each.
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5857491193 7592041488').split()] # For Multiple Id Use One Space Between Each.
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]  # For Multiple Id Use One Space Between Each.
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
